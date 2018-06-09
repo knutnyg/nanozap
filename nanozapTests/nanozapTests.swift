85cb79d754ce3f344c3eb780be97e351a7a561b1
Knut Nygaard <knutnyg@gmail.com>
2018-06-09 21:12:15 +0200

    Fastlane setup

--- nanozapTests/nanozapTests.swift
+++ nanozapTests/nanozapTests.swift
@@ -20,7 +20,7 @@ class nanozapTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+    	
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
