c6c5cc7794dfdd1c497e7bebd47d4154b888a81c
Eivind Siqveland Larsen <eivind@jotta.no>
2018-06-09 22:21:16 +0200

    add 1password button

--- nanozap/viewcontrollers/AuthViewController.swift
+++ nanozap/viewcontrollers/AuthViewController.swift
@@ -8,12 +8,14 @@
 
 import Foundation
 import UIKit
+import PasswordExtension
 
 class AuthViewController : UIViewController {
     
     @IBOutlet weak var certTextView: UITextView!
     @IBOutlet weak var macaroonTextView: UITextView!
     @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var onePasswordButton: UIButton!
     
     var macaroonStore:MacaroonStore!
     var certStore:CertStore!
@@ -27,9 +29,30 @@ class AuthViewController : UIViewController {
         
         self.macaroon = macaroonStore.getMacaroon()
         self.cert = certStore.getCert()
-        
+
         macaroonTextView.text = self.macaroon ?? "no macaroon stored"
         certTextView.text = self.cert ?? "no cert stored"
+    }
+    
+    @IBAction func onePasswordButtonClicked(_ sender: UIButton) {
+        // Using the provided classes
+        PasswordExtension.shared.findLoginDetails(for: "https://github.com/lnd", viewController: self, sender: nil) { (loginDetails, error) in
+            if let loginDetails = loginDetails {
+                print("Title: \(loginDetails.title ?? "")")
+                print("Username: \(loginDetails.username)")
+                print("Password: \(loginDetails.password ?? "")")
+                print("Notes: \(loginDetails.notes ?? "")")
+                print("URL: \(loginDetails.urlString ?? "")")
+                print("Fields: \(loginDetails.fields ?? "")")
+            } else if let error = error {
+                switch error.code {
+                case .extensionCancelledByUser:
+                    print(error.localizedDescription)
+                default:
+                    print("Error: \(error)")
+                }
+            }
+        }
     }
     
     @IBAction func click(_ sender: UIButton) {
