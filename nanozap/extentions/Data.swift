19bc6b53739655069a949ce81bcb2aa888ff820b
Eivind Siqveland Larsen <eivind@jotta.no>
2018-06-16 00:15:52 +0200

    Optional: add some helpers

--- nanozap/extentions/Data.swift
+++ nanozap/extentions/Data.swift
@@ -8,6 +8,29 @@
 
 import Foundation
 
+extension Optional {
+    /// Return the value of the Optional or the `default` parameter
+    /// - param: The value to return if the optional is empty
+    func or(_ other: Wrapped) -> Wrapped {
+        switch self {
+        case .none:
+            return other
+        case .some(let value):
+            return value
+        }
+    }
+
+    /// Returns the unwrapped value of the optional *or*
+    /// the result of calling the closure `else`
+    /// I.e. optional.or(else: {
+    /// ... do a lot of stuff
+    /// })
+    func or(else: () -> Wrapped) -> Wrapped {
+        return self ?? `else`()
+    }
+
+}
+
 extension String {
     /*
      Truncates string to the specified length number of characters and appends an optional trailing string if longer.
