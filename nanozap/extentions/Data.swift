1347708637eac1c6263384f6c1ef896ed051f29b
Eivind Siqveland Larsen <eivind@jotta.no>
2018-06-15 09:26:25 +0200

    String: add truncate ext

--- nanozap/extentions/Data.swift
+++ nanozap/extentions/Data.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+extension String {
+    /*
+     Truncates string to the specified length number of characters and appends an optional trailing string if longer.
+     - Parameter length: Desired maximum lengths of a string
+     - Parameter trailing: A 'String' that will be appended after the truncation.
+
+     - Returns: 'String' object.
+    */
+    func truncate(length: Int, trailing: String = "…") -> String {
+        return (self.count > length) ? self.prefix(length) + trailing : self
+    }
+}
+
 extension Data {
     var bytes: [UInt8] {
         return withUnsafeBytes { (bytes: UnsafePointer<UInt8>) in
